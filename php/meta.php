786a373bc437106ea9760442292f04c983f1a464
PasdeloupNathan <gamee8678@gmail.com>
2022-03-15 13:57:32 +0100

    linked main page

--- php/meta.php
+++ php/meta.php
@@ -11,7 +11,7 @@ function template_meta($title) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/header.css">
+        <link rel="stylesheet" href="../css/prunelle.css">
     </head>
 
     <body>
@@ -35,4 +35,13 @@ function template_header($title) {
 EOT;
 }
 
+function template_headerEmpty($title) {
+    echo<<<EOT
+
+    <header>
+
+
+    </header>
+EOT;
+}
 ?>
