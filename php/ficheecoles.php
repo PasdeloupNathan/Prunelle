c6928461dcabee523002f88bad13c9226e5dfdb1
PasdeloupNathan <gamee8678@gmail.com>
2022-03-17 11:20:09 +0100

    .

--- php/ficheecoles.php
+++ php/ficheecoles.php
@@ -4,5 +4,23 @@
    template_meta('Fiches écoles');
 
    template_header('Ficheecoles');
+?>
 
+<section class="ficheecoles">
+
+<h1>Fiches écoles</h1>
+<div class="content5 row">
+    <div class="col-5"></div>
+    <div class="form5 col-3">
+        <form action="#" method="post">
+            <label id="lab6" for="codeecole">Code de l'école</label>
+            <input id="inp5" type="text" placeholder="Code de l'école">
+        </form>
+        <button id="b6">Afficher</button>
+    </div>
+</div>
+
+</section>
+
+<?php
 ?>
