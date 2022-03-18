4e7881b2943a5896ac64880b114266957a976869
PasdeloupNathan <gamee8678@gmail.com>
2022-03-18 11:17:33 +0100

    ..

---
+++ php/defmdp.php
@@ -0,0 +1,32 @@
+
+   <?php
+   include 'meta.php';
+
+   template_meta('Administrateur');
+
+   template_headerEmpty('Admin');
+?>
+
+<section class="defmdp align-items-center">
+
+<H1>Définition du mot de passe</H1>
+
+<div class="row form">
+    <div class="col-4"></div>
+    <form action="#" method="post" class="col-4">
+        <label for="id">Mot de passe</label>
+        <div class="input-group">
+            <input type="Id" placeholder="Identifiant" name="loginId" />
+        </div>
+        <label for="password">Confirmation mot de passe</label>
+        <div class="input-group">
+            <input type="password" placeholder="Mot de passe" name="loginPassword" />
+        </div>
+        <button id="submit" type="submit" class="login-button">Enregistrerkn </button>
+    </form>
+</div>
+</section>
+
+
+<?php
+?>
