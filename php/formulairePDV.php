4e93a107ef35085b949b1f1a5214fbfb481a87bc
PasdeloupNathan <pasdeloupnathan@gmail.com>
2022-03-31 12:07:24 +0200

    mid day

--- php/formulairePDV.php
+++ php/formulairePDV.php
@@ -5,12 +5,37 @@
 
    template_header('Formulaires');
 ?>
-    <section class="formPDV align-items-center">
+     <section class="formPDV">
 
-        <H1>Formulaires Prise De Vue</H1>
+<h1>Formulaire Prise De Vue</h1>
 
-            
-        </section>
+<div class="formContent1 row">
+
+
+    <div class="col-5"></div>
+    <div class="col-3 form7">
+
+        <form action="#" method="post">
+            <label for="photographename" id="lab9">Nom du photographe 1</label>
+            <select name="" id=""></select>
+            <label for="photographename" id="lab9">Nom du photographe 2</label>
+            <select name="" id=""></select>
+            <label for="duréePDV" id="lab9">Durée de PDV en heure</label>
+            <input type="text" placeholder="Nombres d'heures">
+            <label for="écolename" id="lab9">Nom de l'école</label>
+            <input type="text" placeholder="Nom de l'école">
+        </form>
+        <button id="b8">Enregistrer</button>
+
+    </div>
+
+</div>
+
+</section>
+
+</body>
+
+</html>
 
 <?php
 ?>
