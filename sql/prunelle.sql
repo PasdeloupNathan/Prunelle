d9072901d1ba5b2cecfe31384076fb821a796331
PasdeloupNathan <66208884+PasdeloupNathan@users.noreply.github.com>
2022-04-14 11:28:44 +0200

    Update prunelle.sql

--- sql/prunelle.sql
+++ sql/prunelle.sql
@@ -13,19 +13,40 @@ GO
 
 
 CREATE TABLE `user` (
-
+  `ID` int(11) NOT NULL,
+  `USERNAME` varchar(320) NOT NULL,
+  `MAIL` varchar(320) NOT NULL,
+  `PASSWORD` varchar(128) NOT NULL,
 )
 
 
 CREATE TABLE `ecole` (
+  `CODEECOLE` int(11) NOT NULL,
 
 )
 
 CREATE TABLE `intervenant` (
-
+  `ID` int(11) NOT NULL,
+  `NOM` varchar(320 NOT NULL,
 )
 
 CREATE TABLE `produit` (
-    
+  `ID` int(11) NOT NULL,
+
 )
 
+-- CREATE TABLE `PDV` (
+
+-- )
+-- CREATE TABLE `EMPOCHAGE` (
+
+-- )
+-- CREATE TABLE `OUTPOST` (
+
+-- )
+-- CREATE TABLE `LIVRAISON` (
+
+-- )
+-- CREATE TABLE `FACTURE` (
+
+-- )
